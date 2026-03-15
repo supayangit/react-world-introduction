5169daae1470c6ef83589276465394f99730ad78
supayangit <supayanchk@gmail.com>
2026-03-15 12:55:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Countries from './components/countries'
+
+function App() {
+
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const countries = () => {
+    return (
+        <div>
+            <h1>In the Countries</h1>
+        </div>
+    );
+};
+
+export default countries;
