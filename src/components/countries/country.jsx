71bfeeac2d62a8f2acd1d4c1e541894a0d5a7242
supayangit <supayanchk@gmail.com>
2026-03-15 17:50:35 +0600

    conditional css and button toggle

--- src/components/countries/country.jsx
+++ src/components/countries/country.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../countries/country.css'
 
-const country = ({ country }) => {
+const Country = ({ country }) => {
+    const [visited, setVisited] = useState(false);
+
     const handleVisited = () => {
-        console.log("button clicked");
+        setVisited(!visited);
     }
 
 
     return (
-        <div className="country">
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <h5>Capital: {country.capital.capital}</h5>
             <h5>Area: {country.area.area}, {country.area.area > 3000 ? "Large Country" : "Small Country"}</h5>
             <p>Population: {country.population.population}</p>
 
-            <button onClick={handleVisited}>Not visited</button>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
 
-export default country;
+export default Country;
